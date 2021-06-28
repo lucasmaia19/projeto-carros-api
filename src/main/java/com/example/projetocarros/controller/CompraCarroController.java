a4fde861d4be83b9df1f60b17af5e802fceabb85
lucasmaia19 <lucasmaiaalves19@gmail.com>
2021-06-28 16:44:18 -0300

    deploy how travis

--- src/main/java/com/example/projetocarros/controller/CompraCarroController.java
+++ src/main/java/com/example/projetocarros/controller/CompraCarroController.java
@@ -98,7 +98,7 @@ public class CompraCarroController {
 //				System.out.println("data futura invalido");
 
 //				throw new Exception();
-				throw new InvalidAlgorithmParameterException("Data futura invalida");
+				throw new InvalidAlgorithmParameterException("Data futura INVALIDA");
 			} else {
 
 			}
